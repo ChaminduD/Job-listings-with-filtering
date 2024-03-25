9941fc6d2288100f8caf99bc6c6e155234a2781d
ChaminduD <chamindudulanjana10@gmail.com>
2024-03-25 14:02:52 +0530

    Add React Vite

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Job from './Components/Job/Job';
+import Footer from './Components/Footer/Footer';
+
+function App() {
+
+  return (
+    <>
+      <Job/>
+      <Footer />
+    </>
+  )
+}
+
+export default App
